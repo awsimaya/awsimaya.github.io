8ef899c6dfcc42e682a700555bf3161a7aa408a8
Imaya Kumar Jagannathan <ijaganna@amazon.com>
2025-05-10 16:34:49 -0400

    Updates

---
+++ precache-manifest.6950fd915909d996787e6b7e4c3f3bfa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f06d279e6498422426b81a192c1dd103",
+    "url": "/index.html"
+  },
+  {
+    "revision": "52a3609cb7d8ba9de87c",
+    "url": "/static/css/main.3733ea40.chunk.css"
+  },
+  {
+    "revision": "c5a97ff7e125e3e0b4b5",
+    "url": "/static/js/2.743f1c04.chunk.js"
+  },
+  {
+    "revision": "2d816b829ab53b2d5b7c246891b1b2c1",
+    "url": "/static/js/2.743f1c04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66a27acd62d0bf909caf",
+    "url": "/static/js/3.7a2089af.chunk.js"
+  },
+  {
+    "revision": "52a3609cb7d8ba9de87c",
+    "url": "/static/js/main.41615061.chunk.js"
+  },
+  {
+    "revision": "b27b749cdddd6c4115ac",
+    "url": "/static/js/runtime-main.fea5a25c.js"
+  }
+]);
